3f7dbabf0fbcca1eb37e0dd3c3be8bd0d577cad8
Goyal <gaurav.d.goyal@accenture.com>
2018-06-06 17:50:19 +0530

    new Post code

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -7,6 +7,11 @@ class Posts extends Component {
   componentWillMount() {
     this.props.fetchPosts();
   }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.newPost) {
+      this.props.posts.unshift(nextProps.newPost);
+    }
+  }
   render() {
     const postItems = this.props.posts.map(post => (
       <div key={post.id}>
@@ -25,10 +30,12 @@ class Posts extends Component {
 
 Posts.propTypes = {
   fetchPosts: PropTypes.func.isRequired,
-  posts: PropTypes.array.isRequired
+  posts: PropTypes.array.isRequired,
+  newPost: PropTypes.object
 };
 const mapStateToProps = state => ({
-  posts: state.posts.items // we are getting this post from rootReducer
+  posts: state.posts.items, // we are getting this post from rootReducer
+  newPost: state.posts.item
 });
 export default connect(
   mapStateToProps,
